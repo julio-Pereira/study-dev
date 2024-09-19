f1ba17efefd1d0f648d14872ab295b5860c531b6
julio-Pereira <jandrade.pereira1@gmail.com>
2024-09-19 09:55:18 -0300

    initial commit

---
+++ study-challenges/orange-bank/src/main/java/com/julio/orange_bank/OrangeBankApplication.java
@@ -0,0 +1,13 @@
+package com.julio.orange_bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrangeBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrangeBankApplication.class, args);
+	}
+
+}
---
+++ study-challenges/orange-bank/src/test/java/com/julio/orange_bank/OrangeBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.julio.orange_bank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrangeBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
