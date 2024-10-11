e4364eac33d6db304f005e60b628dd8432ebd8fc
julio-Pereira <jandrade.pereira1@gmail.com>
2024-10-11 09:53:57 -0300

    feat: add TransacaoController.java.java logic

--- study-challenges/orange-bank/src/main/java/com/julio/orange_bank/controller/TransacaoController.java
+++ study-challenges/orange-bank/src/main/java/com/julio/orange_bank/controller/TransacaoController.java
@@ -1,24 +1,43 @@
 package com.julio.orange_bank.controller;
 
-import com.julio.orange_bank.records.Transacao;
+import com.julio.orange_bank.model.Transacao;
+import com.julio.orange_bank.dto.TransacaoDto;
 import com.julio.orange_bank.service.TransacaoService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/transacao")
 public class TransacaoController {
 
     private TransacaoService transacaoService;
+
+
     public TransacaoController(TransacaoService transacaoService) {
         this.transacaoService = transacaoService;
     }
 
-    @PostMapping
-    public void createNewTransaction() {
+    @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<TransacaoDto> createNewTransaction(@RequestBody TransacaoDto transacao) {
+           try {
+               transacaoService.createNewTransaction(transacao);
+               return ResponseEntity.created(null).build();
+           } catch (IllegalArgumentException exception) {
+               return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).build();
+           } catch (Exception exception) {
+               return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+           }
 
+    }
+
+    @DeleteMapping
+    public ResponseEntity deleteTransacoes() {
+        transacaoService.deleteTransacoes();
+        return ResponseEntity.ok(null);
     }
 
     @GetMapping(produces = "application/json")
