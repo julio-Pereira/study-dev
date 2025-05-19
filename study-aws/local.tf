e3543bcf5b2f5b9fc9639720908c1eeed9a851e6
julio-Pereira <jandrade.pereira1@gmail.com>
2025-05-19 19:33:00 -0300

    created dummy terraform config

--- study-aws/local.tf
+++ study-aws/local.tf
@@ -1,5 +1,24 @@
 
 resource "local_file" "exemplo" {
     filename = "exemplo.txt"
-    content  = "Hello, Baby!"
+    content  = var.content
+}
+
+data "local_file" "exemplo" {
+    filename = local_file.exemplo.filename
+}
+
+output "data-source-result" {
+  value = data.local_file.exemplo.content_base64
+}
+variable "content" {
+    type = string
+}
+
+output "file-id" {
+  value = resource.local_file.exemplo.id
+}
+
+output "content" {
+  value = var.content
 }
